f6a28d75de665d8539d3cbb8f27b249fc4c59f6e
Guilherme Veiga Pedromilo <guipedromilo@gmail.com>
2024-03-20 15:12:21 -0300

    update

--- src/components/TouristLocation.js
+++ src/components/TouristLocation.js
@@ -15,7 +15,7 @@ export default function TouristLocation({
     <View>
       <Text style={styleName}>{Name}</Text>
       <ScrollView>
-        <Image style={styleImage} source={ImageSource} />
+        <View style={{alignItems: 'center'}} ><Image style={styleImage} source={ImageSource} /></View>
         <Text style={styleAdress}>{adress}</Text>
         <View style={styleBarra}></View>
         <Text style={styleDescription}>{description}</Text>
